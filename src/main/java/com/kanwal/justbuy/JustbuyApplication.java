ef75f9303d2941593260e36ac3cf774c4c3d4400
Roshan Kanwal <roshan.kanwal03@gmail.com>
2025-04-19 13:18:06 +0530

    Initial commit

---
+++ src/main/java/com/kanwal/justbuy/JustbuyApplication.java
@@ -0,0 +1,13 @@
+package com.kanwal.justbuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustbuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustbuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kanwal/justbuy/JustbuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanwal.justbuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustbuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
